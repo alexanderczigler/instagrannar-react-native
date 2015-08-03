223bc81e77693133bff714442386af62588593a1
ilix <me@ilix.se>
2015-08-03 12:07:17 +0200

    Fixed wrapping of error text

--- Instagrannar/components/Message.js
+++ Instagrannar/components/Message.js
@@ -11,8 +11,10 @@ module.exports = React.createClass({
   render: function() {
     return (
       <View style={styles.messageView}>
-        <Text style={styles.messageHeader}>{this.props.header}</Text>
-        <Text style={styles.messageBody}>{this.props.body}</Text>
+        <View style={styles.messageWrap}>
+          <Text style={styles.messageHeader}>{this.props.header}</Text>
+          <Text style={styles.messageBody}>{this.props.body}</Text>
+        </View>
       </View>
     );
   }
@@ -21,9 +23,12 @@ module.exports = React.createClass({
 
 var styles = StyleSheet.create({
   messageView: {
-    alignItems: 'center',
+    flex: 1,
     marginTop: 180,
-    flex: 1
+    alignItems: 'center'
+  },
+  messageWrap: {
+    width: 300
   },
   messageHeader: {
     fontSize: 20,
@@ -31,7 +36,8 @@ var styles = StyleSheet.create({
     textAlign: 'center'
   },
   messageBody: {
-    fontSize: 16,
+    fontSize: 14,
+    marginTop: 24,
     textAlign: 'center'
   }
 });
