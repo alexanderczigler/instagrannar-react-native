4c87ea53176184456f9b4627d5f45c431e9bcc81
ilix <me@ilix.se>
2015-08-07 13:06:17 +0200

    Rudimentary search result view.

--- Instagrannar/components/Search.js
+++ Instagrannar/components/Search.js
@@ -1,25 +1,71 @@
 var React = require('react-native');
 var RNLocalSearch = require('.././node_modules/react-native-localsearch/RNLocalSearch.ios.js');
 
+var LocationActions = require('../actions/LocationActions');
+
 var {
   Text,
   View,
-  StyleSheet
+  ListView,
+  TextInput,
+  StyleSheet,
+  TouchableHighlight
 } = React;
+
+var __TimeoutId = 0;
 
 module.exports = React.createClass({
 
   getInitialState: function() {
-    return {};
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      searchText: {
+        text: ''
+      }
+    };
   },
 
   render: function() {
-    this._doSearch();
+    console.log('rendering and', this.state);
     return (
-      <View style={styles.searchContainer}>
-        <Text>search...</Text>
+      <View>
+        <View style={styles.searchFieldContainer}>
+          <TextInput style={styles.searchField} onChangeText={(text) => this._onTextChange({text})} value={this.state.searchText.text} />
+        </View>
+        <View style={styles.searchResultContainer}>
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={this._renderRow}
+            style={styles.listView}
+            />
+        </View>
       </View>
     );
+  },
+
+  _renderRow: function(item) {
+    var _onResultPress = this._onResultPress;
+    var onResultPress = function() {
+      _onResultPress(item);
+    }
+    return (
+      <TouchableHighlight activeOpacity={0.8} onPress={onResultPress}>
+        <View>
+          <Text>{item.name}</Text>
+          <Text>{item.title}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  },
+
+  _onTextChange: function(text) {
+    this.setState({searchText: text}, function() {
+      if (text !== '') {
+        this._doSearch();
+      }
+    });
   },
 
   _doSearch: function() {
@@ -30,20 +76,47 @@ module.exports = React.createClass({
       longitudeDelta: 0.1
     };
 
-    var handleResults = function (err, resp) {
-      console.log('results!!!', err, resp);
-    };
-    console.log('R', RNLocalSearch);
     if (RNLocalSearch) {
-      RNLocalSearch.searchForLocations('Lalas', region, handleResults);
+      RNLocalSearch.searchForLocations(this.state.searchText.text, region, this._handleResponse);
     }
+  },
+
+  _handleResponse: function(err, resp) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(resp)
+      });
+    }
+  },
+
+  _onResultPress: function(something) {
+    console.log('something is', something.location);
+    LocationActions.set(something.location);
   }
 
 });
 
 var styles = StyleSheet.create({
-  searchContainer: {
+  searchFieldContainer: {
     flexDirection: 'row',
-    height: 600
+    height: 42
   },
+  searchField: {
+    flex: 1,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  searchResultContainer: {
+    flexDirection: 'row',
+    height: 550
+  },
+  listView: {
+    flex: 1,
+    paddingTop: 0,
+    marginTop: 0,
+  }
 });
