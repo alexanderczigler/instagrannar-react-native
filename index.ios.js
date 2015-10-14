86e77e8a35f7a0a381f9959eccfc8c5046469914
ilix <me@ilix.se>
2015-10-14 21:07:44 +0200

    Removed references to react-native-orientation.

--- index.ios.js
+++ index.ios.js
@@ -2,22 +2,12 @@
 
 var React = require('react-native');
 var Dashboard = require('./components/Dashboard');
-var Orientation = require('react-native-orientation');
 
 var {
   AppRegistry
 } = React;
 
 var Instagrannar = React.createClass({
-
-  componentDidMount: function () {
-    try {
-      console.log(Orientation);
-      Orientation.lockToPortrait();
-    } catch (e) {
-      console.log('ERROR', 'Unable to lock rotation.');
-    }
-  },
 
   render: function () {
     return (
